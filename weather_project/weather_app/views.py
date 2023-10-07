a122c4680b3b646fef7c867fb1b780c2687d6bd0
tarunshrestha <128881726+tarunshrestha@users.noreply.github.com>
2023-10-07 22:06:09 +0545

    Update views.py

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     API_KEY = open("F:\\Projects\\python\\WeatherWebpage\\weather_project\\weather_app\\API_KEY",
-                   "r").read()  # "ae9caee5dafd3fcbef73d876ff281b86"
+                   "r").read()  # ""
     current_weather_url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
     forecast_url = ('https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,'
                     'alerts&appid={}')
